4fc5e25a22eb349d5e583c10321d065b58c4e583
mnaserdoust <mnaserdoust@alix.ai>
2023-03-12 11:06:30 -0400

    fixed center header

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import Home from './Components/Home'
 
 function App() {
   return (
-    <div className="App .m-dark-green">
-      <div className="container">
-        <div className="row justify-content-center h-100">
-          <div className="col-sm-1 h-100">
-            <div className="sticky-bottom">
-              
-            </div>
+    <div className="App m-dark-green">
+      <div className="container  vh-100">
+        <div className="row vh-100">
+          <div className="col-sm-1">
           </div>
-          <div className="col-sm-10">
+          <div className="col-sm-10 align-self-md-center">
           <Home></Home>
-          <div className="fixed-bottom m-light-yellow">...</div>
           </div>
           <div className="col-sm-1"></div>
         </div>
       </div>
+      <div className="fixed-bottom m-light-yellow">
+            <h6 className="">
+              <span className="m-light-yellow">Get in touch </span>
+              <span className="emoji pointer"></span>
+              <span >
+                <a href="mailto:brittany.chiang@gmail.com" target="_blank" className="m-orange" rel="noreferrer">mehrdad.naser@gmail.com</a>
+              </span> 
+            </h6>
+          </div>
     </div>
   );
 }
